312f8b9c690905ecffd37b4f4b29f7ea67092fde
Nati Namvong <nati@flyingcomma.com>
2015-10-10 13:34:35 +0700

    add e2e test

--- spec.js
+++ spec.js
@@ -1,5 +1,7 @@
 describe('Testing App', function () {
   it('should ...', function () {
     browser.get('http://127.0.0.1:8080')
+
+    expect(element(by.css('body')).getText()).toEqual('Nati')
   })
 })
